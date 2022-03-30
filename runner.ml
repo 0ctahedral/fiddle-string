29f573a52a708b20036896db5837f31b3d621de7
Ben Lerner <blerner@ccs.neu.edu>
2022-03-30 18:06:45 -0400

    wrong handling of heap size args

--- runner.ml
+++ runner.ml
@@ -165,7 +165,15 @@ type compile_opts = { valgrind: bool; no_builtins: bool; heap_size: int option }
 let starts_with target src =
   String.length src >= String.length target &&
     String.sub src 0 (String.length target) = target
+    ;;
+
+let chomp str =
+  if str = "" then str 
+  else if str.[String.length str - 1] = '\n'
+  then String.sub str 0 (String.length str - 1)
+  else str
 ;;
+
 let read_options filename : compile_opts =
   let opts =
     if Sys.file_exists filename then String.split_on_char '\n' (string_of_file filename) else [] in
@@ -176,6 +184,15 @@ let read_options filename : compile_opts =
     no_builtins = List.mem "no_builtins" opts;
     heap_size = heap_size
   }
+;;
+
+let parse_args (argsfile: string) (opts: compile_opts) : string list =
+  if Sys.file_exists argsfile then
+    String.split_on_char '\n' (chomp (string_of_file argsfile))
+  else
+    match opts.heap_size with
+    | Some(size) -> [string_of_int size]
+    | None -> []
 ;;
 
 let test_run ?no_builtins:(no_builtins=false) ?args:(args=[]) ?std_input:(std_input="") program_str outfile expected ?cmp:(cmp=(=)) test_ctxt =
@@ -240,7 +257,7 @@ let test_does_run filename test_ctxt =
   let infile   = sprintf "input/do_pass/%s.in" filename in
   let opts     = read_options (sprintf "input/do_pass/%s.options" filename) in
   let prog = string_of_file progfile in
-  let args = if Sys.file_exists argsfile then String.split_on_char '\n' (chomp (string_of_file argsfile)) else [] in
+  let args = parse_args argsfile opts in
   let output = if Sys.file_exists outfile then chomp (string_of_file outfile) else "" in
   let input = if Sys.file_exists infile then (string_of_file infile) else "" in
   let runner = if opts.valgrind then test_run_valgrind else test_run in
@@ -259,7 +276,7 @@ let test_does_err filename test_ctxt =
   let infile   = sprintf "input/do_err/%s.in" filename in
   let opts     = read_options (sprintf "input/do_err/%s.options" filename) in
   let prog = string_of_file progfile in
-  let args = if Sys.file_exists argsfile then String.split_on_char '\n' (chomp (string_of_file argsfile)) else [] in
+  let args = parse_args argsfile opts in
   let err = if Sys.file_exists errfile then chomp (string_of_file errfile) else "" in
   let input = if Sys.file_exists infile then (string_of_file infile) else "" in
   test_err ~no_builtins:opts.no_builtins ~args:args ~std_input:input prog ("do_err/" ^ filename) err ~vg:opts.valgrind test_ctxt
@@ -273,7 +290,7 @@ let test_doesnt_run filename test_ctxt =
   let infile   = sprintf "input/dont_pass/%s.in" filename in
   let opts     = read_options (sprintf "input/dont_pass/%s.options" filename) in
   let prog = string_of_file progfile in
-  let args = if Sys.file_exists argsfile then String.split_on_char '\n' (chomp (string_of_file argsfile)) else [] in
+  let args = parse_args argsfile opts in
   let input = if Sys.file_exists infile then (string_of_file infile) else "" in
   let runner = if opts.valgrind then run_vg else run_no_vg in
 
@@ -296,7 +313,7 @@ let test_doesnt_err filename test_ctxt =
   let infile   = sprintf "input/dont_err/%s.in" filename in
   let opts     = read_options (sprintf "input/dont_err/%s.options" filename) in
   let prog = string_of_file progfile in
-  let args = if Sys.file_exists argsfile then String.split_on_char '\n' (string_of_file argsfile) else [] in
+  let args = parse_args argsfile opts in
   let input = if Sys.file_exists infile then (string_of_file infile) else "" in
   let runner = if opts.valgrind then run_vg else run_no_vg in
 

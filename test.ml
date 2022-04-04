0add0c254999ada405d75fae0b86b6e28f703e1c
Morgan Rosenkranz <rosenkranz1999@gmai.com>
2022-04-04 10:32:33 -0400

    added tests from Fer-De-Lance

--- test.ml
+++ test.ml
@@ -35,9 +35,94 @@ let teq name actual expected = name>::fun _ ->
 
 let builtins_size = 4 (* arity + 0 vars + codeptr + padding *) * 1 (* TODO FIXME (List.length Compile.native_fun_bindings) *)
 
-let simple = [
+let simple_tests = [
   t "five" "5" "" "5";
+  t "simple_let" "let x = 5 in x" "" "5";
+  t "simple_prim1" "let x = 5 in add1(x)" "" "6";
+  t "simple_prim2" "let x = 3, y = x + 5 in y * 2" "" "16";
 ]
+
+let program_tests = [
+  t "simple" "let x = 5 in x" "" "5";
+  t "number5" "5" "" "5";
+  t "tuple" "let t = (0, 5) in t" "" "(0, 5)";
+  t "thruple" "let t = (0, 3, 5) in t" "" "(0, 3, 5)";
+  t "thruple_get" "let t = (0, 3, 5) in t[0]" "" "0";
+  terr "too_high" "let t = (0, 3, 5) in t[7]" "" "index too large to get, got 7";
+  terr "too_low" "let t = (0, 3, 5) in t[-3]" "" "index too small to get, got -3";
+
+  t "nil" "let t = nil in t" "" "nil";
+  terr "deref_nil" "let t = nil in t[0]" "" "tried to access component of nil";
+
+  t "set" "let t = (0, 3) in t[0] := 10" "" "10";
+  t "set_confirm" "let t = (0, 3) in let x = t[0] := 10 in t" "" "(10, 3)";
+  t "set_get" "let t = (0, 3) in let x = t[0] := 10 in t[0]" "" "10";
+
+  t "blank" "let _ = print((0, 2 + 1)) in 5" "" "(0, 3)\n5";
+
+  t "nested" "let t = (0, (2, 1)) in t" "" "(0, (2, 1))";
+  t "double_nested" "let t = ((2, 1), (4, 5)) in t" "" "((2, 1), (4, 5))";
+
+  t"desugar_bindlist" "let (a, b, c) = (0, 3, 5) in c" "" "5";
+  t"bindlist_destructure_nested" "let (a, (b, c)) = (0, (3, 5)) in (a, b, c)" "" "(0, 3, 5)";
+  t"bindlist_keep_structure" "let (a, b) = (0, (3, 5)) in b" "" "(3, 5)";
+  t "eq1" "nil == nil" "" "true";
+  t "eq2" "nil == ()" "" "false";
+  t "eq3" "() == ()" "" "false";
+  t "eq4" "true == (3, 4)" "" "false";
+  t "let" "let x = 5, y = 6 in let z = 10 in x * y + z" "" "40";
+  t "tuples_let" "let t = (1, 2) in let (a, b) = t, m = 3 in a + (b * m)" "" "7";
+  t "stack_alloc1" "let x = 5 in let y = 6 in let z = y in let f = (lambda: x + z) in let h = (lambda: z + y) in f()" "" "11";
+  t "stack_alloc2" "let a = 1, b = 2, c = 3, d = 4, e = 5 in let f = (lambda: a + e) in f()" "" "6";
+];;
+
+let type_tests = [ 
+  t "isbool_true" "isbool(true)" "" "true";
+  t "isbool_false" "isbool(false)" "" "true";
+  t "isbool_num" "isbool(5)" "" "false";
+  t "isbool_tuple" "isbool((5, 3))" "" "false";
+  t "isbool_nil" "isbool(nil)" "" "false";
+  t "isbool_empty" "isbool(())" "" "false";
+
+  t "istuple_nil" "istuple(nil)" "" "true";
+  t "istuple_empty" "istuple(())" "" "true";
+  t "istuple_tuple" "istuple((5, 3))" "" "true";
+  t "istuple_num" "istuple(5)" "" "false";
+  t "istuple_true" "istuple(true)" "" "false";
+  t "istuple_false" "istuple(false)" "" "false";
+
+  t "isnum_nil" "isnum(nil)" "" "false";
+  t "isnum_num" "isnum(5)" "" "true";
+  t "isnum_true" "isnum(true)" "" "false";
+  t "isnum_false" "isnum(false)" "" "false";
+  t "isnum_tuple" "isnum((5, 3))" "" "false"; 
+
+  t "test_is_bool2" "isbool(false)" "" "true";
+  t "test_is_bool3" "isbool(0)" "" "false";
+  t "test_is_bool4" "isbool(123)" "" "false";
+  t "test_is_bool5" "isbool((0,123))" "" "false";
+  t "test_is_bool6" "isbool((true,123))" "" "false";
+  t "test_is_bool7" "isbool((123,123))" "" "false";
+  t "test_is_bool8" "isbool((false,123))" "" "false";
+
+  t "test_is_tuple1" "istuple(true)" "" "false";
+  t "test_is_tuple2" "istuple(false)" "" "false";
+  t "test_is_tuple3" "istuple(0)" "" "false";
+  t "test_is_tuple4" "istuple(123)" "" "false";
+  t "test_is_tuple5" "istuple((0,123))" "" "true";
+  t "test_is_tuple6" "istuple((true,123))" "" "true";
+  t "test_is_tuple7" "istuple((123,123))" "" "true";
+  t "test_is_tuple8" "istuple((false,123))" "" "true";
+
+  t "test_is_num1" "isnum(true)" "" "false";
+  t "test_is_num2" "isnum(false)" "" "false";
+  t "test_is_num3" "isnum(0)" "" "true";
+  t "test_is_num4" "isnum(123)" "" "true";
+  t "test_is_num5" "isnum((0,123))" "" "false";
+  t "test_is_num6" "isnum((true,123))" "" "false";
+  t "test_is_num7" "isnum((123,123))" "" "false";
+  t "test_is_num8" "isnum((false,123))" "" "false";
+];;
 
 let pair_tests = [
   t "tup1" "let t = (4, (5, 6)) in
@@ -55,12 +140,18 @@ let pair_tests = [
               t[1] := t;
               t
             end" "" "(4, <cyclic tuple 1>)";
+  t "tup5" "let t = (4, (5, (6, nil))) in
+            begin
+              t[1][1][1] := t;
+              t
+            end" "" "(4, (5, (6, <cyclic tuple 1>)))";
   t "tup4" "let t = (4, 6) in
             (t, t)"
            ""
-           "((4, 6), (4, 6))"
+           "((4, 6), (4, 6))";
 
-]
+  terr "tup_get_not_num" "let x = (1, 2, 3) in let y = (0, 6) in x[y]" "" "expected numeric index";
+];;
 
 let oom = [
   tgcerr "oomgc1" (7 + builtins_size) "(1, (3, 4))" "" "Out of memory";
@@ -87,15 +178,16 @@ let input = [
     t "input1" "let x = input() in x + 2" "123" "125"
   ]
 
-
-let suite =
-"unit_tests">:::
- (* pair_tests @ oom @ gc @ input*)
-  simple
-
-
-
+let program_suite = "program_tests">:::program_tests;;
+let type_suite = "type_tests">:::type_tests;;
+let pair_suite = "pair_tests">:::pair_tests;;
+let simple_suite = "pair_tests">:::simple_tests;;
 
 let () =
-  run_test_tt_main ("all_tests">:::[suite; input_file_test_suite ()])
+  run_test_tt_main ("all_tests">:::[
+    simple_suite;
+    pair_suite;
+    program_suite;
+    type_suite;
+    input_file_test_suite ()])
 ;;
